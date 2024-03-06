7a7cd4fd3750757da44afafdb98a93d908c99a0e
Cody Oram <cody.oram@keyin.com>
2024-03-06 14:10:32 -0330

    March5th

--- Classes/Week10/first-app/src/App.js
+++ Classes/Week10/first-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let firstName = "Cody";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My First react progject</h1>
+      <p>React is fun to learn!!!</p>
+      <p>this is going to be a jsx thing...</p>
+      <p>Welcome {firstName}</p>
+      <p>Welcome {2 + 2}</p>
     </div>
   );
 }
 
 export default App;
+
+// Code that was in the div before i changed it...
+//<header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+// <p>
+// Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+// Learn React
+// </a>
+//</header>

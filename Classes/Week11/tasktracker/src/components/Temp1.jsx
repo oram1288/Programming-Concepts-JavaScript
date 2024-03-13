35a145c15a0408fdd76a743712c5b0d53c0713d9
Cody Oram <cody.oram@keyin.com>
2024-03-13 14:12:01 -0230

    march13th

---
+++ Classes/Week11/tasktracker/src/components/Temp1.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Temp1 = () => {
+  let [num, setNum] = useState(0);
+
+  let [name, setName] = useState("Cody Oram");
+
+  function handleClick() {
+    setNum(num + 1);
+    setName("Bob Smith");
+  }
+  return (
+    <div>
+      <h1>Use State Hook</h1>
+      <p>
+        {num} - {name}
+      </p>
+      <button onClick={handleClick}>Change Number</button>
+    </div>
+  );
+};
+
+export default Temp1;

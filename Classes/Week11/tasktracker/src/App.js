0c04b0042e040214fcbe3d2922d96c28eca42402
Cody Oram <cody.oram@keyin.com>
2024-03-15 17:11:15 -0230

    march15th

--- Classes/Week11/tasktracker/src/App.js
+++ Classes/Week11/tasktracker/src/App.js
@@ -3,17 +3,41 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import Temp1 from "./components/Temp1";
 import Temp2 from "./components/Temp2";
+import Temp3 from "./components/Temp3";
+import { useState } from "react";
 
 function App() {
+  let [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "School App",
+      day: "March 18th at 3:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Gym",
+      day: "March 20th at 2:30pm",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "Interview App",
+      day: "March 21th at 1:30pm",
+      reminder: true,
+    },
+  ]);
+
   const onClick = (e) => {
     console.log("click from App component");
   };
   return (
     <div className="container">
-      {/* <Header title="Task Tracker" onClick={onClick} />
-      <Tasks />
-      <Temp1 /> */}
-      <Temp2 />
+      <Header title="Task Tracker" onClick={onClick} />
+      <Tasks tasks={tasks} />
+      {/* <Temp1 /> */}
+      {/* <Temp2 /> */}
+      {/* <Temp3 /> */}
     </div>
   );
 }
